d2a35a2804fbbe27fdd0c8343630445805f7a0b0
SUZUKIyuka <suzukiyuka1218@gmail.com>
2014-11-14 09:51:11 +0900

    initial commit

---
+++ DivideImageSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DivideImageSample
+//
+//  Created by nagata on 11/14/14.
+//  Copyright (c) 2014 nagata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DivideImageSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DivideImageSample
+//
+//  Created by nagata on 11/14/14.
+//  Copyright (c) 2014 nagata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
