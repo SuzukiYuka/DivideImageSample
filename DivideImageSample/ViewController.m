8269df3f7540b6a7cd3a5462f7577744693b7237
SUZUKIyuka <suzukiyuka1218@gmail.com>
2014-12-26 07:17:50 +0900

    refactor

--- DivideImageSample/ViewController.m
+++ DivideImageSample/ViewController.m
@@ -20,63 +20,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    //
-//    views = [[NSMutableArray alloc] init];
-    size = 50;
-    // サンプル画像を読み込む
-    UIImage *image_mae = [UIImage imageNamed:@"mizubuu.png"];
+    size = 50; // パズルピースの画像の大きさ
+    UIImage *image_mae = [UIImage imageNamed:@"mizubuu.png"]; // サンプル画像を読み込む
     UIImage *image;  // リサイズ後UIImage
     CGFloat width = 200;  // リサイズ後幅のサイズ
     CGFloat height = 200;  // リサイズ後高さのサイズ
     
+    /* リサイズする */
     UIGraphicsBeginImageContext(CGSizeMake(width, height));
     [image_mae drawInRect:CGRectMake(0, 0, width, height)];
     image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
 
- 
-    //画像を分割->buttonへ
-    NSArray *buttons = [self divideImage:image];
+    NSArray *buttons = [self divideImage:image]; // 画像を分割->buttonへ
     
+    /* パズルピース(button)を画面に追加 */
     for (UIButton *bt in buttons) {
         [self.view addSubview:bt];
-       // [views addObject:bt];
     }
     
+    /* ピースの最後は表示させない */
     UIButton *lastbtn = [buttons lastObject];
-   // lastbtn.hidden = YES;
-    
+    lastbtn.hidden = YES;
     
 }
 
+/* ピース（分割した画像が背景のボタン）をつくる */
 - (NSArray *)divideImage:(UIImage *)image
 {
-    // イメージをバラバラに分割する
     NSMutableArray *result = [[NSMutableArray alloc] init];
-    //ひとつひとつ分かれた画像の大きさ
+    
     int i = 0;
     for (int y=0; y<200; y+=size) {
         for (int x=0; x<200; x+=size) {
-//            UIImage *croppedImage = [self imageByCropping:image toRect:CGRectMake(x, y, size, size)];
-            UIButton *b = [[UIButton alloc] initWithFrame:CGRectMake(x+60, y+100, size, size)];  // ボタンのサイズを指定する
+            // 各ピース(button)をつくる
+            UIButton *b = [[UIButton alloc] initWithFrame:CGRectMake(x+60, y+100, size, size)];
+            // 背景を分割したピースの画像にする
             [b setBackgroundImage:[self imageByCropping:image][i] forState:UIControlStateNormal];
-            i++;
-        //    b.layer.zPosition = - (y * 100 + x); // 重なったとき上に来るように
+            //ボタンを押したときのアクションを追加
             [b addTarget:self action:@selector(pushed:) forControlEvents:UIControlEventTouchUpInside];
-
+            i++;
             [result addObject:b];
         }
     }
-
     return result;
 }
 
+/* 画像を分割する */
 - (NSArray *)imageByCropping:(UIImage *)crop
 {
     NSMutableArray *croppedImages = [[NSMutableArray alloc] init];
     for (int y=0; y<200; y+=size) {
         for (int x=0; x<200; x+=size) {
-            // 指定した四角でイメージを切り抜き
+            // 指定した四角でイメージをトリミング
             CGRect rect = CGRectMake(x, y, size, size);
             CGImageRef imageRef = CGImageCreateWithImageInRect([crop CGImage], rect);
             UIImage *cropped = [UIImage imageWithCGImage:imageRef];
@@ -84,29 +80,30 @@
             [croppedImages addObject:cropped];
         }
     }
-    //画像の順番をランダムにする
+    /* 画像の順番をランダムにする */
     int count = [croppedImages count];
     for (int i = count - 1; i > 0; i--) {
         int randomNum = arc4random() % i;
         [croppedImages exchangeObjectAtIndex:i withObjectAtIndex:randomNum];
+        NSLog(@"croppedImages == %@",croppedImages);
     }
     return croppedImages;
 }
 
 
-//puzzleの動き
+/* ピースが押されたときの動き */
 -(void)pushed:(UIButton*)button{
     //まわりにボタンの空きスペースがあるか
     
     //画像が元の画像と一致しているか
 }
-//まわりの空きスペースを確認
+/* まわりの空きスペースを確認 */
 -(void)checkHiddenBtn{
     
 }
-//完成かどうかの判定
--(void)check{
-    
+/* 完成かどうか判定する */
+-(BOOL)check{
+    return NO;
 }
 
 - (void)didReceiveMemoryWarning {
